42030b0f9a8b5d394672cf75009cccec6af51517
Lärsle <Lars@Malach.de>
2014-10-27 09:10:39 +0100

    [CLEANUP] Removes unused variables from ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -1,10 +1,6 @@
 <?php
 defined('TYPO3_MODE') or exit();
 
-$extPath    = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY);
-$extRelPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY);
-
-
 // ############################################################################
 // Backend
 // ############################################################################
